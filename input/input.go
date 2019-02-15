8d8af4341bf489fd9fc90a6ec9d489deb3c103cb
Alexander Lazarov <alexander.lazaroff@gmail.com>
2019-02-15 21:30:22 +0200

    Handle simple input

---
+++ input/input.go
@@ -0,0 +1,19 @@
+package input
+
+import (
+	"github.com/alexander-lazarov/lightriders/models"
+	"github.com/hajimehoshi/ebiten"
+	"github.com/hajimehoshi/ebiten/inpututil"
+)
+
+func HandleInput(board *models.Board) {
+	if inpututil.IsKeyJustPressed(ebiten.KeyUp) {
+		board.SetDirP1(models.Up)
+	} else if inpututil.IsKeyJustPressed(ebiten.KeyDown) {
+		board.SetDirP1(models.Down)
+	} else if inpututil.IsKeyJustPressed(ebiten.KeyRight) {
+		board.SetDirP1(models.Right)
+	} else if inpututil.IsKeyJustPressed(ebiten.KeyRight) {
+		board.SetDirP1(models.Right)
+	}
+}
--- main.go
+++ main.go
@@ -4,15 +4,29 @@ import (
 	"time"
 
 	"github.com/alexander-lazarov/lightriders/graphics"
+	"github.com/alexander-lazarov/lightriders/input"
 	"github.com/alexander-lazarov/lightriders/models"
 	"github.com/hajimehoshi/ebiten"
 )
 
 func update(screen *ebiten.Image) error {
+	input.HandleInput(&board)
 	graphics.UpdateBoardImage(&board)
 	screen.DrawImage(graphics.GetBoardImage(), &ebiten.DrawImageOptions{})
 
 	return nil
+}
+
+func boardLoop() {
+	var winner models.Winner = models.NoWinner
+
+	for {
+		time.Sleep(1000 * time.Millisecond)
+
+		if winner == models.NoWinner {
+			winner, _ = board.Advance()
+		}
+	}
 }
 
 var board models.Board
@@ -21,17 +35,7 @@ func main() {
 	board = *models.InitialBoard()
 	graphics.InitBoardImage()
 
-	go func() {
-		var winner models.Winner = models.NoWinner
-
-		for {
-			time.Sleep(1000 * time.Millisecond)
-
-			if winner == models.NoWinner {
-				winner, _ = board.Advance()
-			}
-		}
-	}()
+	go boardLoop()
 
 	ebiten.Run(update, 320, 240, 2, "Hello world!")
 }
